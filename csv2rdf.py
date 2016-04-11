89e74e7451fe4e89948ae92ba0f604be4451c38d
natereed <reedn@dnb.com>
2016-04-11 14:14:07 -0500

    Updates

--- csv2rdf.py
+++ csv2rdf.py
@@ -4,6 +4,9 @@ from tripleutils import print_triple_literal
 
 COMPANY_URI = 'http://dnb.com/duns/%s'
 
+def map_key_to_predicate(key):
+    return 'http://dnb.com/company/beneficial-ownership-attributes/' + key
+
 with open("sample.csv") as csv_in:
     reader = CleanDictReader(csv_in, delimiter='|')
     for row in reader:
@@ -12,7 +15,10 @@ with open("sample.csv") as csv_in:
         del row['subj_duns_nbr']
 
         for key in row.keys():
-            predicate = 'http://dnb.com/company/beneficial-ownership-attributes/' + key
-            print_triple_literal(company_uri, predicate, row[key])
+            predicate = map_key_to_predicate(key)
+            print_triple_literal(company_uri,
+                                 map_key_to_predicate(key),
+                                 row[key])
+
 
 
